86bbba7257391ffc6cc75da9524abaa2d526ca2c
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2014-06-19 17:34:33 -0400

    changed Data Objects

---
+++ src/main/scala/home/kwyho/ss/finance/daoobj/DataObjects.scala
@@ -0,0 +1,10 @@
+package home.kwyho.ss.finance.daoobj
+
+/**
+ * Created by hok1 on 6/19/14.
+ */
+object DataObjects {
+  val yearHash = Map("2013" -> "t-cP5RjsrrdhW6qxupaT_xg", "2014" -> "tsXZvQtYkoE11jq-b1wxpYA")
+  val calendarMonths = List("January", "February", "March", "April", "May", "June", "July", "August", "September",
+    "October", "November", "December")
+}
