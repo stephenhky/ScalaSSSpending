3723cddd55dcc03ff36c213d2c66afd9fa5d9a60
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2015-02-19 11:42:15 -0500

    updated hashid

--- src/main/scala/home/kwyho/ss/finance/daoobj/SSSpendDAO.scala
+++ src/main/scala/home/kwyho/ss/finance/daoobj/SSSpendDAO.scala
@@ -4,8 +4,9 @@ package home.kwyho.ss.finance.daoobj
  * Created by hok1 on 6/19/14.
  */
 object SSSpendDAO {
-  val yearHash = Map("2013" -> "t-cP5RjsrrdhW6qxupaT_xg", "2014" -> "tsXZvQtYkoE11jq-b1wxpYA",
-    "2015" -> "tLrg3di8rs6V8W4sMSqhTaw")
+  val yearHash = Map("2013" -> "1Sr_3CBxyc8NGCdPOXCqMVxeFe5zj1LDXUWdrCeMP6WE",
+    "2014" -> "1EyG00bkrAuursG43Fo_KIsM3sPg3Lg4E0SI4AzkQOow",
+    "2015" -> "1yKTRenG_CmCxmV4hHRwE-RXeC55RH7ahFpOjQ_rON6E")
   val calendarMonths = List("January", "February", "March", "April", "May", "June", "July", "August", "September",
     "October", "November", "December")
   val DataColumnHashMap : Map[Int, String] = Map(2 -> "Date", 3 -> "Place", 4 -> "Category", 5 -> "City", 6 -> "Debit",
