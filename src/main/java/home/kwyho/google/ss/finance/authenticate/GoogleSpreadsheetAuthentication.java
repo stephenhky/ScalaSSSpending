a044393eaa6e81a4397c4538a1db77ba4746acc8
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2015-05-28 17:51:18 -0400

    updating authentication method in progress

--- src/main/java/home/kwyho/google/ss/finance/authenticate/GoogleSpreadsheetAuthentication.java
+++ src/main/java/home/kwyho/google/ss/finance/authenticate/GoogleSpreadsheetAuthentication.java
@@ -3,6 +3,8 @@ package home.kwyho.google.ss.finance.authenticate;
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
 import com.google.gdata.util.AuthenticationException;
 
+// no longer supported by Google. Refer to https://stackoverflow.com/questions/30483601/create-spreadsheet-using-google-spreadsheet-api-in-google-drive-in-java?newreg=e6435ad6891a4c1f8bbe76f4fb22fd64
+@Deprecated
 public class GoogleSpreadsheetAuthentication {
 	public static SpreadsheetService login(String username, String password) throws AuthenticationException {
 		SpreadsheetService service = new SpreadsheetService("SSSpend");
---
+++ src/main/scala/home/kwyho/ss/finance/authenticate/GoogleSpreadsheetOAuth2Authentication.scala
@@ -0,0 +1,11 @@
+package home.kwyho.ss.finance.authenticate
+
+import com.google.gdata.client.spreadsheet.SpreadsheetService
+
+// New way of authentication. Refer to https://stackoverflow.com/questions/30483601/create-spreadsheet-using-google-spreadsheet-api-in-google-drive-in-java?newreg=e6435ad6891a4c1f8bbe76f4fb22fd64
+object GoogleSpreadsheetOAuth2Authentication {
+  def login(username : String, password : String) : SpreadsheetService = {
+//    val httpTransport : HttpTransport = new
+    null
+  }
+}
