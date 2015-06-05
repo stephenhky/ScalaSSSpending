20d08fbd17a43614b311a629a82d68c710f9d6f1
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2015-06-05 17:58:58 -0400

    progress

--- src/main/scala/home/kwyho/ss/finance/authenticate/GoogleSpreadsheetOAuth2Authentication.scala
+++ src/main/scala/home/kwyho/ss/finance/authenticate/GoogleSpreadsheetOAuth2Authentication.scala
@@ -1,26 +1,29 @@
 package home.kwyho.ss.finance.authenticate
 
-import java.io.{File, InputStream, InputStreamReader}
+import java.io.{InputStream, InputStreamReader}
 import java.util
 
-import com.google.api.client.auth.oauth2.{Credential, AuthorizationCodeFlow}
-import com.google.api.client.extensions.appengine.datastore.AppEngineDataStoreFactory
-import com.google.api.client.googleapis.auth.oauth2.{GoogleAuthorizationCodeFlow, GoogleClientSecrets}
+import com.google.api.client.auth.oauth2.{TokenResponse, Credential, AuthorizationCodeFlow}
+import com.google.api.client.googleapis.auth.oauth2.{GoogleTokenResponse, GoogleAuthorizationCodeFlow, GoogleClientSecrets}
 import com.google.api.client.json.JsonFactory
 import com.google.api.client.json.jackson.JacksonFactory
 
 import com.google.api.client.http.HttpTransport
 import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.gdata.client.authn.oauth.{OAuthSigner, OAuthHmacSha1Signer, GoogleOAuthParameters}
 import com.google.gdata.client.spreadsheet.SpreadsheetService
 
 // New way of authentication. Refer to https://stackoverflow.com/questions/30483601/create-spreadsheet-using-google-spreadsheet-api-in-google-drive-in-java?newreg=e6435ad6891a4c1f8bbe76f4fb22fd64
 // http://stackoverflow.com/questions/13257163/java-google-api-analytics-unable-to-obtain-new-access-token-using-refresh
 // https://github.com/google/google-api-java-client#Authorization_Code_Flow
 // Available scopes: https://developers.google.com/gmail/api/auth/scopes
+// Access token request: https://developers.google.com/identity/protocols/OAuth2InstalledApp
+// Answer at bottom: http://stackoverflow.com/questions/12521385/how-to-authenticate-google-drive-without-requiring-the-user-to-copy-paste-auth-c
 object GoogleSpreadsheetOAuth2Authentication {
 
   def login(username : String, password : String) : SpreadsheetService = {
     val SCOPES = util.Arrays.asList("https://spreadsheets.google.com/feeds", "https://docs.google.com/feeds")
+    val REDIRECT_URI : String = "http://localhost"
 
     val jsonFactory : JsonFactory = new JacksonFactory()
     val httpTransport : HttpTransport = new NetHttpTransport()
@@ -31,8 +34,11 @@ object GoogleSpreadsheetOAuth2Authentication {
     val authorizationCodeFlow : AuthorizationCodeFlow = new GoogleAuthorizationCodeFlow.Builder(
       httpTransport, jsonFactory, clientsSecrets, SCOPES
     ).setAccessType("offline").setApprovalPrompt("auto").build()
-//      .setDataStoreFactory(AppEngineDataStoreFactory.getDefaultInstance)
-    val credential : Credential = authorizationCodeFlow.loadCredential(username)
+    val url : String = authorizationCodeFlow.newAuthorizationUrl().setRedirectUri(REDIRECT_URI).build()
+    println(url)
+    var code: String = readLine("code = ? ")
+    val response : TokenResponse = authorizationCodeFlow.newTokenRequest(code).setRedirectUri(REDIRECT_URI).execute()
+    val credential : Credential = authorizationCodeFlow.loadCredential(username).setFromTokenResponse(response)
 
 
     val service : SpreadsheetService = new SpreadsheetService("ScalaSSSpend")
