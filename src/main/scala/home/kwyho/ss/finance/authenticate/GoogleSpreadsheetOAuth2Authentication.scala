d6893f8341e6a37ba91c8e6363a4d53a774627fa
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2015-06-07 19:16:53 -0400

    delete deprecated file, working authorization code

--- src/main/scala/home/kwyho/ss/finance/authenticate/GoogleSpreadsheetOAuth2Authentication.scala
+++ src/main/scala/home/kwyho/ss/finance/authenticate/GoogleSpreadsheetOAuth2Authentication.scala
@@ -3,17 +3,17 @@ package home.kwyho.ss.finance.authenticate
 import java.io.{InputStream, InputStreamReader}
 import java.util
 
-import com.google.api.client.auth.oauth2.{TokenResponse, Credential, AuthorizationCodeFlow}
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
-import com.google.api.client.googleapis.auth.oauth2.{GoogleTokenResponse, GoogleAuthorizationCodeFlow, GoogleClientSecrets}
+import com.google.api.client.auth.oauth2.{TokenResponse, AuthorizationCodeFlow}
+import com.google.api.client.googleapis.auth.oauth2.{GoogleCredential, GoogleAuthorizationCodeFlow, GoogleClientSecrets}
 import com.google.api.client.json.JsonFactory
 import com.google.api.client.json.jackson.JacksonFactory
 
 import com.google.api.client.http.HttpTransport
 import com.google.api.client.http.javanet.NetHttpTransport
-import com.google.gdata.client.authn.oauth.{OAuthSigner, OAuthHmacSha1Signer, GoogleOAuthParameters}
 import com.google.gdata.client.spreadsheet.SpreadsheetService
+
+import scala.io.{Codec, Source}
+import scala.util.parsing.json._
 
 // New way of authentication. Refer to https://stackoverflow.com/questions/30483601/create-spreadsheet-using-google-spreadsheet-api-in-google-drive-in-java?newreg=e6435ad6891a4c1f8bbe76f4fb22fd64
 // http://stackoverflow.com/questions/13257163/java-google-api-analytics-unable-to-obtain-new-access-token-using-refresh
@@ -26,14 +26,23 @@ object GoogleSpreadsheetOAuth2Authentication {
   def login(username : String) : SpreadsheetService = {
     val SCOPES = util.Arrays.asList("https://spreadsheets.google.com/feeds", "https://docs.google.com/feeds")
     val REDIRECT_URI : String = "http://localhost"
-//    val REDIRECT_URI : String = "urn:ietf:wg:oauth:2.0:oob"
 
+    // define initial object
     val jsonFactory : JsonFactory = new JacksonFactory()
     val httpTransport : HttpTransport = new NetHttpTransport()
 
+    // dealing Google JSON file
     val jsonStream : InputStream = GoogleSpreadsheetOAuth2Authentication.getClass.getResourceAsStream("client_secret_32485935939-p43vo057gp5mdp9cu1k03qcfudk7lv2g.apps.googleusercontent.com.json")
-    val clientsSecrets : GoogleClientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(jsonStream))
+    val jsonStream1 : InputStream = GoogleSpreadsheetOAuth2Authentication.getClass.getResourceAsStream("client_secret_32485935939-p43vo057gp5mdp9cu1k03qcfudk7lv2g.apps.googleusercontent.com.json")
+    val jsonStr : String = Source.fromInputStream(jsonStream1)(Codec.UTF8).getLines().reduce((s1, s2) => s1+s2)
+    val jsonObj : Any = JSON.parseFull(jsonStr)
+    val jsonMap : Map[String, String] = (jsonObj match {
+      case Some(m: Map[String, Map[String, String]]) => m.get("web").get
+      case None => Map()
+    })
 
+    // Google OAuth 2.0 authorization flow
+    val clientsSecrets : GoogleClientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(jsonStream))
     val authorizationCodeFlow : AuthorizationCodeFlow = new GoogleAuthorizationCodeFlow.Builder(
       httpTransport, jsonFactory, clientsSecrets, SCOPES
     ).setAccessType("offline").setApprovalPrompt("auto").build()
@@ -41,8 +50,11 @@ object GoogleSpreadsheetOAuth2Authentication {
     println(url)
     var code: String = readLine("code = ? ")
     val response : TokenResponse = authorizationCodeFlow.newTokenRequest(code).setRedirectUri(REDIRECT_URI).execute()
-    println(response)
-    val credential : Credential = authorizationCodeFlow.loadCredential(username).setFromTokenResponse(response)
+    val credential : GoogleCredential = new GoogleCredential.Builder().setTransport(httpTransport)
+                                          .setJsonFactory(jsonFactory)
+                                          .setClientSecrets(jsonMap("client_id"), jsonMap("client_secret"))
+                                          .build()
+                                          .setFromTokenResponse(response)
 
     val service : SpreadsheetService = new SpreadsheetService("ScalaSSSpend")
     service.setOAuth2Credentials(credential)
