fde66c0d27de4558786e93ca1e9d65cddbfda53e
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2015-06-26 16:33:14 -0400

    single out important comments for future codes to reuse access tokens

--- src/main/scala/home/kwyho/ss/finance/authenticate/GoogleSpreadsheetOAuth2Authentication.scala
+++ src/main/scala/home/kwyho/ss/finance/authenticate/GoogleSpreadsheetOAuth2Authentication.scala
@@ -15,12 +15,14 @@ import com.google.gdata.client.spreadsheet.SpreadsheetService
 import scala.io.{Codec, Source}
 import scala.util.parsing.json._
 
-// New way of authentication. Refer to https://stackoverflow.com/questions/30483601/create-spreadsheet-using-google-spreadsheet-api-in-google-drive-in-java?newreg=e6435ad6891a4c1f8bbe76f4fb22fd64
+// New way of authentication. Refer to
 // http://stackoverflow.com/questions/13257163/java-google-api-analytics-unable-to-obtain-new-access-token-using-refresh
 // https://github.com/google/google-api-java-client#Authorization_Code_Flow
 // Available scopes: https://developers.google.com/gmail/api/auth/scopes
 // Access token request: https://developers.google.com/identity/protocols/OAuth2InstalledApp
-// Answer at bottom: http://stackoverflow.com/questions/12521385/how-to-authenticate-google-drive-without-requiring-the-user-to-copy-paste-auth-c
+//
+// About reusing access token:
+//    http://stackoverflow.com/questions/12521385/how-to-authenticate-google-drive-without-requiring-the-user-to-copy-paste-auth-c
 object GoogleSpreadsheetOAuth2Authentication {
 
   def login(username : String, clientSecretJsonFile : File) : SpreadsheetService = {
@@ -32,7 +34,6 @@ object GoogleSpreadsheetOAuth2Authentication {
     val httpTransport : HttpTransport = new NetHttpTransport()
 
     // dealing Google JSON file
-//    val jsonStream1 : InputStream = GoogleSpreadsheetOAuth2Authentication.getClass.getResourceAsStream("client_secret_32485935939-p43vo057gp5mdp9cu1k03qcfudk7lv2g.apps.googleusercontent.com.json")
     val jsonStr : String = Source.fromFile(clientSecretJsonFile)(Codec.UTF8).getLines().reduce((s1, s2) => s1+s2)
     val jsonObj : Any = JSON.parseFull(jsonStr)
     val jsonMap : Map[String, String] = (jsonObj match {
