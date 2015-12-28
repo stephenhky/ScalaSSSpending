9c74953dc6197d6123ccb943486e758d30211147
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2015-12-28 15:40:36 -0500

    added year 2016

--- src/main/scala/home/kwyho/ss/finance/daoobj/SSSpendDAO.scala
+++ src/main/scala/home/kwyho/ss/finance/daoobj/SSSpendDAO.scala
@@ -8,7 +8,8 @@ import scala.collection.JavaConverters._
 object SSSpendDAO {
   val yearHash = Map("2013" -> "1Sr_3CBxyc8NGCdPOXCqMVxeFe5zj1LDXUWdrCeMP6WE",
     "2014" -> "1EyG00bkrAuursG43Fo_KIsM3sPg3Lg4E0SI4AzkQOow",
-    "2015" -> "1yKTRenG_CmCxmV4hHRwE-RXeC55RH7ahFpOjQ_rON6E")
+    "2015" -> "1yKTRenG_CmCxmV4hHRwE-RXeC55RH7ahFpOjQ_rON6E",
+    "2016" -> "10sdQuxWqy9xKxekvtyeHDIPsRp3DR55LJPff1pL86YQ")
   val calendarMonths = List("January", "February", "March", "April", "May", "June", "July", "August", "September",
     "October", "November", "December")
   val DataColumnHashMap : Map[Int, String] = Map(2 -> "Date", 3 -> "Place", 4 -> "Category", 5 -> "City", 6 -> "Debit",
